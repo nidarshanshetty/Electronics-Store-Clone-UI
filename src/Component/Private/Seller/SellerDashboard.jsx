2a6eb682769d5556c5e11e152982b68d983a43b3
nidarshanshetty <nidarshanshetty97@gmail.com>
2024-02-14 23:26:58 +0530

    commited register operation

---
+++ src/Component/Private/Seller/SellerDashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SellerDashboard() {
+  return (
+    <div className='bg-red-300 '>SellerDashboard</div>
+  )
+}
+
+export default SellerDashboard
---
+++ src/Component/Private/Seller/SellerOrders.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SellerOrders() {
+  return (
+    <div>SellerOrders</div>
+  )
+}
+
+export default SellerOrders
